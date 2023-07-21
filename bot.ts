e7d7ce6449916b02df39082c939121860f0039c0
songzheming <songzheming@163.com>
2023-07-21 18:08:27 +0800

    refator: add qastep session

--- bot.ts
+++ bot.ts
@@ -9,6 +9,7 @@ interface SessionData {
   isSell: boolean;
   qMessageIds: number[];
   aMessageIds: number[];
+  qaStep:number
   initialMessageId: number;
   message: string;
 }
@@ -28,6 +29,7 @@ export function creatBot(token: string) {
     session({
       initial() {
         return {
+          qaStep:0,
           qMessageIds: [],
           aMessageIds: [],
           initialMessageId: null,
@@ -48,14 +50,13 @@ function initBot(bot: Bot<MyContext, Api<RawApi>>) {
     const message = await ctx.reply(initialText, { reply_markup: startMenu });
     ctx.session.initialMessageId = message.message_id;
   });
-  const qaStep = new Map<number, number>();
 bot.on('message:text', async ctx => {
   // QA message
   if (ctx.message.reply_to_message?.message_id) {
     const id = ctx.message.from.id;
-    if (!qaStep.has(id)) qaStep.set(id, 0);
+    ctx.session.qaStep
     const text = ctx.message.text!;
-    const step = qaStep.get(id)!;
+    const step =  ctx.session.qaStep;
     switch (step) {
       case 0:
         const regex_0 = /^[a-zA-Z]{1,8}$/;
@@ -72,11 +73,11 @@ bot.on('message:text', async ctx => {
           }
           ctx.session.qMessageIds = [];
           ctx.session.message = '';
-          qaStep.set(id, 0);
+          ctx.session.qaStep=0
           await ctx.reply(`回复的文字必须为长度不超过8位的英文字符 !`);
           return;
         }
-        qaStep.set(id, 1);
+        ctx.session.qaStep=1
         ctx.session.message += '\n' + text;
         const message = await ctx.reply('Hi, 再按要求回复第二次', {
           reply_markup: { force_reply: true },
@@ -99,11 +100,11 @@ bot.on('message:text', async ctx => {
           }
           ctx.session.qMessageIds = [];
           ctx.session.message = '';
-          qaStep.set(id, 0);
+          ctx.session.qaStep=0
           await ctx.reply(`回复内容必须是长度为42位, 0x开头, 由数字和字母组成的字符串 !`);
           return;
         }
-        qaStep.set(id, 2);
+        ctx.session.qaStep=2
         ctx.session.message += '\n' + text;
         ctx.session.aMessageIds.push(ctx.message.message_id);
         const aMessageIds = ctx.session.aMessageIds;
