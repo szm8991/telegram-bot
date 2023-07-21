ec42b31ef12f23d66b4fbf8f5c018cd386d71446
songzheming <songzheming@163.com>
2023-07-21 12:38:51 +0800

    update: add local proxy

--- bot.ts
+++ bot.ts
@@ -1,24 +1,28 @@
 import dotenv from "dotenv";
 import { Bot } from "grammy";
+import { SocksProxyAgent } from "socks-proxy-agent";
+
+const socksAgent = new SocksProxyAgent('socks://127.0.0.1:7890');
 dotenv.config()
+
 if(!process.env.BOT_TOKEN) {
-    console.warn('-----------------------------------')
-    console.warn('process.env.TOEKN is not set')
-    console.warn('-----------------------------------')
+    console.log('-----------------------------------')
+    console.log('process.env.TOEKN is not set')
+    console.log('-----------------------------------')
     process.exit(1);
 }
-// Create an instance of the `Bot` class and pass your bot token to it.
-const bot = new Bot(process.env.BOT_TOKEN); // <-- put your bot token between the ""
-// You can now register listeners on your bot object `bot`.
-// grammY will call the listeners when users send messages to your bot.
+const bot = new Bot('6362887251:AAE48GI87EQekcGtweaSqNCzgBizNdMuxVs',{
+    client: {
+        baseFetchConfig: {
+          agent: socksAgent,
+          compress: true,
+        },
+      },
+});
 
-// Handle the /start command.
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-// Handle other messages.
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
+bot.on("message:text", (ctx) => ctx.reply("Echo: " + ctx.message.text));
 
-// Now that you specified how to handle messages, you can start your bot.
-// This will connect to the Telegram servers and wait for messages.
-
-// Start the bot.
 bot.start();
+
+console.log('-------------------------start-----------------------------')
